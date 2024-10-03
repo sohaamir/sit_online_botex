aa527f8a965ca5a574430e021281a924a3833a64
Aamir Sohail <axs2210@student.bham.ac.uk>
2024-10-03 15:14:06 +0100

    added apps backin settings.py

--- settings.py
+++ settings.py
@@ -62,7 +62,7 @@ BOTS_CHECK_COMPLETE = False
 SESSION_CONFIGS = [
      dict(
          name='social_influence_task',  # Unique identifier for this session configuration
-         app_sequence=['instructions', 'submission'],  # Order of apps in the experiment
+         app_sequence=['instructions', 'practice_task', 'main_task_instructions', 'waiting_room', 'main_task', 'player_left', 'submission'],  # Order of apps in the experiment
          num_demo_participants=5,  # Number of demo participants, useful for testing
          # use_browser_bots=True,  # Uncomment to use bots for testing
          # Prolific completion links for different scenarios
