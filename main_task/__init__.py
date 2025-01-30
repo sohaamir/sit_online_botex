ea3d06d255d843e860a6d950c688eb5130df45b5
Aamir Sohail <axs2210@student.bham.ac.uk>
2025-01-30 17:32:08 +0000

    changed timeout to 10 minutes for deployment

--- main_task/__init__.py
+++ main_task/__init__.py
@@ -859,7 +859,7 @@ class WaitPage2(WaitPage):
     def vars_for_template(player):
         return {
             'title_text': 'Waiting for Other Players',
-            'timeout_seconds': 30  # 10 minutes
+            'timeout_seconds': 600  # 10 minutes
         }
     
     @staticmethod              
